86670e245e707a4247b256068ec35daa2dbe65e7
Grace-luning-fu <grace.luning.fu@gmail.com>
2017-07-27 12:10:45 -0400

    shorten condistions

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -5,36 +5,36 @@ public class FizzBuzz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		/**
-		 * This is a program that prints the numbers from 1 to 100. But for
-		 * multiples of three print “Fizz” instead of the number and for the
-		 * multiples of five print “Buzz”. For numbers which are multiples of
-		 * both three and five print “FizzBuzz”
+		 * This is a program that prints the numbers from 1 to 100. 
+		 * But for multiples of 3, print “Fizz” instead of the number; 
+		 * for multiples of 5, print “Buzz”. 
+		 * for numbers which are multiples of both 3 and 5, print “FizzBuzz”
 		 * 
 		 * @author Grace
 		 * 
 		 ***/
 		// loop from number 1 to 100
 		for (int counter = 1; counter <= 100; counter++) {
+			
+			//Initialize an empty string 
+			String PrintOut = "";
+		
 			// If the number is a multiple of 3, display "Fizz"
-			if (counter % 3 == 0 && counter % 5 != 0) {
-				System.out.println("Fizz!");
+			if (counter % 3 == 0) {
+				PrintOut += "Fizz";
 			}
 
 			// If the number is a multiple of 5, display "Buzz"
-			else if (counter % 5 == 0 && counter % 3 != 0) {
-				System.out.println("Buzz!");
-
+			if (counter % 5 == 0) {
+				PrintOut += "Buzz";
 			}
 
-			// If the number is a multiple of both 3 and 5, display "FizzBuzz"
-			else if (counter % 5 == 0 && counter % 3 == 0) {
-				System.out.println("FizzBuzz!");
-
-			}    
-			// print the number if neither
-			else {
-				System.out.println(counter);
+			// If not the above conditions, convert the integer into string and print the integer as string
+			if(counter % 3 !=0 && counter % 5 !=0){
+				PrintOut = "" + counter; 
 			}
+			
+			System.out.println(PrintOut);
 		}
 
 	}
